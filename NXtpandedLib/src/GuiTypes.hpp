86c1f63c26ec8e59bd708cce3daac8c9c1c0e941
martin-au <m.aumair@gmx.net>
2014-10-21 18:16:32 +0200

    Auto formatting

--- NXtpandedLib/src/GuiTypes.hpp
+++ NXtpandedLib/src/GuiTypes.hpp
@@ -5,32 +5,54 @@
  *      Author: Martin Aumair
  */
 
-
 #ifndef __GUITYPES_HPP_
 #define __GUITYPES_HPP_
 
 /** \file
  *	\ingroup NxtLcd
-*/
+ */
 
 namespace nxpl {
 
 /**
  * \brief Draw Options for functions.
  */
-class DrawOpt {
+class DrawOpt
+{
 private:
 	S8 opt;
-	explicit DrawOpt(S8 op) : opt(op) {};
+	explicit DrawOpt(S8 op)
+			: opt(op)
+	{
+	}
+
 public:
 	static const S8 drawID = 1;
 	static const S8 clearID = 0;
 	static const S8 invertID = -1;
 
-	static DrawOpt draw() {return DrawOpt(drawID);}     /**<Normal drawing */
-	static DrawOpt clear() {return DrawOpt(clearID);}   /**<Clear pixels while drawing (aka draw in white) */
-	static DrawOpt invert() {return DrawOpt(invertID);} /**<Invert pixels*/
-	const S8 operator()(void) const {return opt;}       /**<Get ID of the draw option (used for switch/compare)*/
+	/*! Normal drawing */
+	static DrawOpt draw()
+	{
+		return DrawOpt(drawID);
+	}
+	/*! Clear pixels while drawing (aka draw in white) */
+	static DrawOpt clear()
+	{
+		return DrawOpt(clearID);
+	}
+
+	/*! Invert pixels*/
+	static DrawOpt invert()
+	{
+		return DrawOpt(invertID);
+	}
+
+	/*! Get ID of the draw option (used for switch/compare)*/
+	const S8 operator()(void) const
+	{
+		return opt;
+	}
 };
 
 }
