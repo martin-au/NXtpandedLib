02548939de3cb1f8bbc068568d40f57b4e2c4da2
martin-au <m.aumair@gmx.net>
2014-10-23 21:55:26 +0200

    Added NXtpandedLib templates

---
+++ NXtpandedLib/Templates/withTimer/sample.cpp
@@ -0,0 +1,42 @@
+/* sample.cpp for TOPPERS/ATK(OSEK) */ 
+
+// fix this paths
+extern "C" {
+#include "../../../toppers_osek/include/kernel.h"
+#include "kernel_id.h"
+#include "../../../ecrobot/c/ecrobot_interface.h"
+}
+
+DeclareCounter(SysTimerCnt);
+
+// C++ Includes and objects should be defined here.
+// fix this paths also
+#include "../../src/NTimer.hpp"
+
+extern "C" {
+// startup/shutdown hooks
+void ecrobot_device_initialize(void);
+void ecrobot_device_terminate(void);
+
+// nxtOSEK hook to be invoked from an ISR in category 2
+void user_1ms_isr_type2(void){
+	SleeperMonitor();
+	StatusType ercd;
+
+	ercd = SignalCounter(SysTimerCnt); /* Increment OSEK Alarm Counter */
+	if (ercd != E_OK) {
+	    ShutdownOS(ercd);
+	}
+}
+
+TASK(TaskMain)
+{
+	nxpl::NTimer timer;
+	while(1) {
+		timer.wait(1000);
+	}
+	TerminateTask();
+}
+
+}
+
