cd600a3165d7b1c21d11780be8f8af0800d54cae
martin-au <m.aumair@gmx.net>
2015-01-05 11:17:21 +0100

    Fixed NOstream example.

--- NXtpandedLib/Examples/NOstream/sample.cpp
+++ NXtpandedLib/Examples/NOstream/sample.cpp
@@ -1,48 +1,27 @@
 /* sample.cpp for TOPPERS/ATK(OSEK) */ 
 
-// fix this paths
 extern "C" {
 #include "../../../toppers_osek/include/kernel.h"
-#include "kernel_id.h"
-#include "../../../ecrobot/c/ecrobot_interface.h"
 }
-
-
-DeclareCounter(SysTimerCnt);
-DeclareResource(OstreamResource);
 
 // C++ Includes and objects should be defined here.
-// fix this paths also
 #include "../../../NXtpandedLib/src/NOstream.hpp"
-#include "../../../NXtpandedLib/src/NTimer.hpp"
-
-nxpl::NMutex ostreamMtx(OstreamResource);
-nxpl::NOstream cout(ostreamMtx);
+#include "../../../NXtpandedLib/src/NNxt.hpp"
 
 extern "C" {
-// startup/shutdown hooks
-void ecrobot_device_initialize(void);
-void ecrobot_device_terminate(void);
 
-// nxtOSEK hook to be invoked from an ISR in category 2
-void user_1ms_isr_type2(void){
-	SleeperMonitor();
-	StatusType ercd;
-
-	ercd = SignalCounter(SysTimerCnt); /* Increment OSEK Alarm Counter */
-	if (ercd != E_OK) {
-	    ShutdownOS(ercd);
-	}
-}
+// nxtOSEK 1ms interrupt
+void user_1ms_isr_type2(void){}
 
 TASK(TaskMain)
 {
-	cout << "Hello world" << nxpl::endl;
-	cout << "Hello world\n";
-	cout << "Hello world" << "\n";
+    cout << "Hello world 1" << endl;
+	cout << "Hello world 2\n";
+	cout << "Hello world 3" << "\n";
 	cout.flush(true);
 
-	TerminateTask();
+	NNxt::wait(5000);
+	NNxt::restart();
 }
 
 }
