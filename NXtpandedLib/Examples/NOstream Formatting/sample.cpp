ccd199dbfe17dc4cb6922bf3e4b2bea307feb45f
martin-au <m.aumair@gmx.net>
2015-01-05 11:29:20 +0100

    Fixed NOstreamFormatting example.

--- NXtpandedLib/Examples/NOstream Formatting/sample.cpp
+++ NXtpandedLib/Examples/NOstream Formatting/sample.cpp
@@ -1,39 +1,17 @@
 /* sample.cpp for TOPPERS/ATK(OSEK) */ 
 
-// fix this paths
 extern "C" {
 #include "../../../toppers_osek/include/kernel.h"
-#include "kernel_id.h"
-#include "../../../ecrobot/c/ecrobot_interface.h"
 }
-
-
-DeclareCounter(SysTimerCnt);
-DeclareResource(OstreamResource);
 
 // C++ Includes and objects should be defined here.
-// fix this paths also
 #include "../../../NXtpandedLib/src/NOstream.hpp"
-#include "../../../NXtpandedLib/src/NTimer.hpp"
-
-nxpl::NMutex ostreamMtx(OstreamResource);
-nxpl::NOstream cout(ostreamMtx);
+#include "../../../NXtpandedLib/src/NNxt.hpp"
 
 extern "C" {
-// startup/shutdown hooks
-void ecrobot_device_initialize(void);
-void ecrobot_device_terminate(void);
 
-// nxtOSEK hook to be invoked from an ISR in category 2
-void user_1ms_isr_type2(void){
-	SleeperMonitor();
-	StatusType ercd;
-
-	ercd = SignalCounter(SysTimerCnt); /* Increment OSEK Alarm Counter */
-	if (ercd != E_OK) {
-	    ShutdownOS(ercd);
-	}
-}
+// nxtOSEK 1ms interrupt
+void user_1ms_isr_type2(void){}
 
 TASK(TaskMain)
 {
@@ -41,7 +19,7 @@ TASK(TaskMain)
 	float myFloatNumber = 32.12345;
 
 	cout << "myIntNum: " << myIntNumber << '\n';
-	cout << "as Hex: " << nxpl::hex << myIntNumber << "\n";
+	cout << "as Hex: " << hex << myIntNumber << "\n";
 
 	cout << "---------------\n";
 
@@ -54,7 +32,8 @@ TASK(TaskMain)
 
 	cout.flush(true);
 
-	TerminateTask();
+	NNxt::wait(5000);
+	NNxt::restart();
 }
 
 }
