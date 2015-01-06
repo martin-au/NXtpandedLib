e215e227e75f93b860ff6f6a0a8b5c539786078a
martin-au <m.aumair@gmx.net>
2015-01-06 11:21:40 +0100

    Fixed NPairBox example

--- NXtpandedLib/Examples/NPairBox/sample.cpp
+++ NXtpandedLib/Examples/NPairBox/sample.cpp
@@ -3,40 +3,15 @@
 // fix this paths
 extern "C" {
 #include "../../../toppers_osek/include/kernel.h"
-#include "kernel_id.h"
-#include "../../../ecrobot/c/ecrobot_interface.h"
 }
-
-DeclareCounter(SysTimerCnt);
-DeclareResource(OstreamResource);
 
 // C++ Includes and objects should be defined here.
-// fix this paths also
-#include "../../../NXtpandedLib/src/NOstream.hpp"
-#include "../../../NXtpandedLib/src/NLabel.hpp"
-#include "../../../NXtpandedLib/src/NPairBox.hpp"
-#include "../../../NXtpandedLib/src/NTimer.hpp"
-
-nxpl::NMutex ostreamMtx(OstreamResource);
-nxpl::NOstream cout(ostreamMtx);
+#include "../../../NXtpandedLib/nxtpandedlib.h"
 
 extern "C" {
-// startup/shutdown hooks
-void ecrobot_device_initialize(void);
-void ecrobot_device_terminate(void);
 
 // nxtOSEK hook to be invoked from an ISR in category 2
-void user_1ms_isr_type2(void){
-	StatusType ercd;
-
-	ercd = SignalCounter(SysTimerCnt); /* Increment OSEK Alarm Counter */
-	if (ercd != E_OK) {
-	    ShutdownOS(ercd);
-	}
-}
-
-
-using namespace nxpl;
+void user_1ms_isr_type2(void){}
 
 TASK(TaskMain)
 {
@@ -49,7 +24,6 @@ TASK(TaskMain)
 	NPairBox pairBox1(&indicator, &indicatorText);
 
 	// now show all align options for the second widget
-	NTimer timer;
 	U8 i = 0;
 	while(true) {
 		switch (i) {
@@ -71,7 +45,7 @@ TASK(TaskMain)
 
 		++i;
 		if(i > 3) i = 0;
-		timer.wait(1000);
+		NNxt::wait(1000);
 	}
 
 	TerminateTask();
