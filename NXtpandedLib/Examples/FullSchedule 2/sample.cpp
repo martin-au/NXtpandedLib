2e1b3dacb14f68c913cc1c194a2be1f37e6a6357
martin-au <m.aumair@gmx.net>
2015-01-17 13:58:13 +0100

    Fixed example

--- NXtpandedLib/Examples/FullSchedule 2/sample.cpp
+++ NXtpandedLib/Examples/FullSchedule 2/sample.cpp
@@ -1,27 +1,17 @@
 /* sample.cpp for TOPPERS/ATK(OSEK) */ 
 
-// fix this paths
 extern "C" {
 #include "../../../toppers_osek/include/kernel.h"
-#include "kernel_id.h"
-#include "../../../ecrobot/c/ecrobot_interface.h"
 }
 
 DeclareCounter(SysTimerCnt);
-DeclareResource(OstreamResource);
 
 // C++ Includes and objects should be defined here.
 // fix this paths also
 #include "../../../NXtpandedLib/src/NOstream.hpp"
-#include "../../../NXtpandedLib/src/NTimer.hpp"
-
-nxpl::NMutex ostreamMtx(OstreamResource);
-nxpl::NOstream cout(ostreamMtx);
+#include "../../nxtpandedlib.h"
 
 extern "C" {
-// startup/shutdown hooks
-void ecrobot_device_initialize(void);
-void ecrobot_device_terminate(void);
 
 // nxtOSEK hook to be invoked from an ISR in category 2
 void user_1ms_isr_type2(void){
@@ -36,32 +26,34 @@ void user_1ms_isr_type2(void){
 
 TASK(TaskMain)
 {
-	nxpl::NTimer timer;
 	cout << "TaskMain start\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << endl;
+	NLcd::update();
 
+	// do something
 	int k = 0;
 	for(int i=0; i<10000; i++) {
 		k = i * i / k;
-		timer.wait(1);
+		NNxt::wait(1);
 	}
 
 	cout << "TaskMain end\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << endl;
+	NLcd::update();
 	TerminateTask();
 }
 
+// Task 2 will interrupt TaskMain multiple times because it has fixed cycle time and
+// higher priority (see .oil file)
 TASK(Task2)
 {
 	cout << "Task2 start\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << endl;
+	NLcd::update();
 
 	cout << "Task2 end\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << endl;
+	NLcd::update();
 	TerminateTask();
 }
 
