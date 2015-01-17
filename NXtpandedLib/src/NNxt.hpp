6133f75abf4fc561bd317ec4ea5835848a53b751
martin-au <m.aumair@gmx.net>
2015-01-17 13:54:45 +0100

    Added sleep function NNxt

--- NXtpandedLib/src/NNxt.hpp
+++ NXtpandedLib/src/NNxt.hpp
@@ -10,6 +10,7 @@
 
 extern "C" {
 #include "../../ecrobot/c/ecrobot_interface.h"
+#include "../../ecrobot/c/rtoscalls.h"
 }
 
 /**
@@ -147,6 +148,48 @@ public:
 	 static void wait(U32 ms)
 	 {
 		 systick_wait_ms(ms);
+	 }
+
+	 /**
+	  * Let the task sleep for ms duration. Lets lower priority tasks execute.
+	  * User needs to implement a C function and RTOS provided events.
+	  *
+	  * User needs to implement a C function and RTOS provided events.<BR><BR>
+	  * + Invoke SleeperMonitor function in user_1msec_isr_type2 hook.<BR>
+	  * &nbsp;&nbsp;void user_1ms_isr_type2(void)<BR>
+	  * &nbsp;&nbsp;{<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;SleeperMonitor(); // needed for I2C devices and Clock class<BR>
+	  * &nbsp;&nbsp;}<BR>
+	  * <BR>
+	  * + Define EventSleepI2C and EventSleep Events in user oil file.<BR>
+	  * &nbsp;&nbsp;EVENT EventSleepI2C<BR>
+	  * &nbsp;&nbsp;{<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;MASK = AUTO;<BR>
+	  * &nbsp;&nbsp;};<BR><BR>
+	  * &nbsp;&nbsp;EVENT EventSleep<BR>
+	  * &nbsp;&nbsp;{<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;MASK = AUTO;<BR>
+	  * &nbsp;&nbsp;};<BR><BR>
+	  * &nbsp;&nbsp;TASK TaskMain<BR>
+	  * &nbsp;&nbsp;{<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;AUTOSTART = TRUE<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;{<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;APPMODE = appmode1;<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;};<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;PRIORITY = 1;<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;ACTIVATION = 1;<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;SCHEDULE = FULL;<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;STACKSIZE = 512;<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;EVENT = EventSleepI2C; <- Here it is<BR>
+	  * &nbsp;&nbsp;&nbsp;&nbsp;EVENT = EventSleep; <- Here it is<BR>
+	  * &nbsp;&nbsp;};<BR>
+	  * <BR>
+	  * [ How to use with TOPPERS/JSP(ITRON) ]<BR>
+	  * In case of using this class with TOPPERS/JSP(ITRON), RTOS specific definitions for the class are not needed.
+	  */
+	 static void sleep(U32 ms)
+	 {
+		 Sleep(ms);
 	 }
 
 private:
