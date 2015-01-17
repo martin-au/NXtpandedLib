937cb45a84be223907fe930480162920d3fd5921
martin-au <m.aumair@gmx.net>
2015-01-17 14:06:26 +0100

    Fixed examples

--- NXtpandedLib/Examples/ResourceProtection/sample.cpp
+++ NXtpandedLib/Examples/ResourceProtection/sample.cpp
@@ -3,8 +3,6 @@
 // fix this paths
 extern "C" {
 #include "../../../toppers_osek/include/kernel.h"
-#include "kernel_id.h"
-#include "../../../ecrobot/c/ecrobot_interface.h"
 }
 
 DeclareCounter(SysTimerCnt);
@@ -12,22 +10,14 @@ DeclareResource(OstreamResource);
 DeclareResource(MyResource);
 
 // C++ Includes and objects should be defined here.
-// fix this paths also
 #include "../../../NXtpandedLib/src/NOstream.hpp"
-#include "../../../NXtpandedLib/src/NTimer.hpp"
-#include "../../../NXtpandedLib/src/Mutex.hpp"
-
-nxpl::NMutex ostreamMtx(OstreamResource);
-nxpl::NOstream cout(ostreamMtx);
+#include "../../../NXtpandedLib/nxtpandedlib.h"
 
 extern "C" {
-// startup/shutdown hooks
-void ecrobot_device_initialize(void);
-void ecrobot_device_terminate(void);
 
 // nxtOSEK hook to be invoked from an ISR in category 2
 void user_1ms_isr_type2(void){
-	SleeperMonitor();
+	SleeperMonitor(); // must be called here if you use sleep function.
 	StatusType ercd;
 
 	ercd = SignalCounter(SysTimerCnt); /* Increment OSEK Alarm Counter */
@@ -39,24 +29,26 @@ void user_1ms_isr_type2(void){
 
 // here we give the resource to a NXtpandedLib Mutex object
 // which makes things easier
-nxpl::NMutex myMutex(MyResource);
+NMutex myMutex(MyResource);
 
 TASK(TaskMain)
 {
-	nxpl::NTimer timer;
 	cout << "TaskMain start\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << "\n";
+	cout.flush(true);
 
-	//myMutex.acquire();
+	// critical section
+	//myMutex.acquire(); // use explicit acquire/release or use lock guard class
 	{
-	nxpl::LockGuard lockGuard(myMutex);
+	LockGuard lockGuard(myMutex); //
 
 	int k = 0;
 	for(int i=0; i<100000; i++) {
 		k = i * i / k;
 		if(i == 100000/2) {
-			timer.sleep(1);
+			// if you take a look at FullSchedule example, here will the other task
+			// get time to execute, in this case it cant because mutex is locked!
+			NNxt::sleep(1);
 		}
 	}
 
@@ -64,8 +56,8 @@ TASK(TaskMain)
 	//myMutex.release();
 
 	cout << "TaskMain end\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << "\n";
+	cout.flush(true);
 	TerminateTask();
 }
 
@@ -74,12 +66,12 @@ TASK(Task2)
 	myMutex.acquire();
 
 	cout << "Task2 start\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << "\n";
+	cout.flush(true);
 
 	cout << "Task2 end\n";
-	cout << systick_get_ms() << nxpl::endl;
-	display_update();
+	cout << NNxt::getTick() << "\n";
+	cout.flush(true);
 
 	myMutex.release();
 
